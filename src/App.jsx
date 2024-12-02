28484bf021229ad9313861ae1e881054fd9b3a26
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-12-02 19:25:18 +0530

    Add Music and Image Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import ImageSection from "./components/ImageSection"
+import Sound from "./components/Sound"
 import Welcome from "./components/Welcome"
 
 function App() {
   return (
     <div>
+      <Sound/>
       <Welcome/>
+      <ImageSection/>
     </div>
   )
 }
---
+++ src/components/ImageSection.jsx
@@ -0,0 +1,19 @@
+import { BackgroundLines } from "./ui/background-lines"
+import { BackgroundGradient } from "./ui/background-gradient";
+function ImageSection() {
+  return (
+    <BackgroundLines className="relative h-[40rem] overflow-hidden flex items-center justify-center">
+      <div>
+        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 text-teal-50">
+          <img
+            src="src\assets\react.svg"
+            alt="Aapka hi photo tha pata nhi kaha gya :'("
+            className="rounded-lg shadow-lg w-80 md:w-96"
+          />
+        </BackgroundGradient>
+      </div>
+    </BackgroundLines>
+  )
+}
+
+export default ImageSection
