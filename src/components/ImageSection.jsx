a66ab71dcdf18f6b00f297d7f1f8c7643e37a497
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-12-04 16:10:36 +0530

    Update ImageSection to use imported image asset

--- src/components/ImageSection.jsx
+++ src/components/ImageSection.jsx
@@ -1,12 +1,14 @@
 import { BackgroundLines } from "./ui/background-lines"
 import { BackgroundGradient } from "./ui/background-gradient";
+import senoraImage from "../assets/senora.jpeg";
+
 function ImageSection() {
   return (
     <BackgroundLines className="relative h-[40rem] overflow-hidden flex items-center justify-center">
       <div>
         <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 text-teal-50">
           <img
-            src="src/assets/senora.jpeg"
+            src={senoraImage}
             alt="Aapka hi photo tha pata nhi kaha gya :'("
             className="rounded-lg shadow-lg"
           />
